e956b7fed78d0e83cfbd3c189748b4d1bfea01a7
paul lahana <paul.lahana@gmail.com>
2020-09-21 17:33:37 +0200

    adding book and cancel + helpers + front

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :machines, only: [:index, :show] do
     resources :bookings, only: [:create]
   end
+  resources :bookings, only: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_071005) do
+ActiveRecord::Schema.define(version: 2020_09_21_140423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_071005) do
   create_table "machines", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.boolean "visible"
+    t.boolean "visible", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
