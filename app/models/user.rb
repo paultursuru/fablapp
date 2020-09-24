748732ae19eb901a7f7029005fcf9b9424aaf6e5
paul lahana <paul.lahana@gmail.com>
2020-09-24 17:58:19 +0200

    fix-dependent-destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :machines, through: :bookings
 
   enum role: [:student, :admin]
