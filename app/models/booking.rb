1d87e6ac8ad6f005694a4dbcba0a75fd9af1b48c
paul lahana <paul.lahana@gmail.com>
2021-06-22 15:18:37 +0200

    fixing upcoming and past bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,11 +4,11 @@ class Booking < ApplicationRecord
 
   validates_uniqueness_of :machine_id, scope: [:start_time]
 
-  scope :in_the_future, -> { where("start_time > ?", DateTime.now) }
-  scope :in_the_past, -> { where("start_time < ?", DateTime.now) }
-  scope :is_today, -> { where("start_time < ?", (Time.now + 1.days).change(hour: 8)) }
-  scope :after_today, -> { where("start_time > ?", (Time.now + 1.days).change(hour: 8)) }
-  scope :before_today, -> { where("start_time < ?", (Time.now).change(hour: 8)) }
-  scope :last_week, -> { where("start_time > ?", (Time.now - 8.days).change(hour: 8)) }
-  scope :is_past_today, -> { where("start_time > ?", (Time.now).change(hour: 8)) }
+  scope :in_the_future, -> { where("start_time > ?", DateTime.now.in_time_zone('Europe/Paris')) }
+  scope :in_the_past, -> { where("start_time < ?", DateTime.now.in_time_zone('Europe/Paris')) }
+  scope :is_today, -> { where("start_time < ?", (Time.now + 1.days).change(hour: 8).in_time_zone('Europe/Paris')) }
+  scope :after_today, -> { where("start_time > ?", (Time.now + 1.days).change(hour: 8).in_time_zone('Europe/Paris')) }
+  scope :before_today, -> { where("start_time < ?", (Time.now).change(hour: 8).in_time_zone('Europe/Paris')) }
+  scope :last_week, -> { where("start_time > ?", (Time.now - 8.days).change(hour: 8).in_time_zone('Europe/Paris')) }
+  scope :is_past_today, -> { where("start_time > ?", (Time.now).change(hour: 8).in_time_zone('Europe/Paris')) }
 end
